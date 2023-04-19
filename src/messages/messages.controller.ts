2c9f4576e0477a6bde8dfb3e5c6654aad2127558
Félix Rioux Sabourin <felix.riouxsabourin@nurun.com>
2023-04-19 14:59:36 -0400

    finish section 5

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,27 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { MessagesService } from './messages.service';
+import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
 
 @Controller('messages')
 export class MessagesController {
+  constructor(public messagesService: MessagesService) {}
+
   @Get()
   listMessages() {
-
+    return this.messagesService.findAll();
   }
 
   @Post()
-  createMessage(@Body() body: CreateMessageDto) {
-    console.log({body})
+  createMessage(@Body() {content}: CreateMessageDto) {
+    return this.messagesService.create(content);
   }
 
   @Get('/:id')
-  getMessage(@Param('id') id: string) {
-    console.log({id})
+  async getMessage(@Param('id') id: string) {
+    const message = await this.messagesService.findOne(id);
+    if (!message) {
+      throw new NotFoundException('Message not found');
+    }
+    return message
   }
 }
